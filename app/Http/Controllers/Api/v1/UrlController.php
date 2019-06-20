755a87e69bfc4751994e4d561b89b8ec7834e59f
Reysmer Valle <reysmerwvr@gmail.com>
2019-06-20 15:09:53 -0500

    New Readme.md | New apidoc.json file | Ignoring public/apidocs
    Modifying Readme.md, new apidoc.json file, removing unused methods in UrlController, ignoring public/apidocs

--- app/Http/Controllers/Api/v1/UrlController.php
+++ app/Http/Controllers/Api/v1/UrlController.php
@@ -76,27 +76,4 @@ class UrlController extends Controller
         $url->increment('visited_count');
         return redirect()->away($url->url);
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
